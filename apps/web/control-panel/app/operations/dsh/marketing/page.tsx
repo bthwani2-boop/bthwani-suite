6574e696bb9b7135a96214d64254a2e8e46c5da6
bthwani2-boop <bthwani2@gmail.com>
2026-04-19 02:56:27 +0300

    chore: update hubHref and operationsHref in DshMarketingPage

--- apps/web/control-panel/app/operations/dsh/marketing/page.tsx
+++ apps/web/control-panel/app/operations/dsh/marketing/page.tsx
@@ -1,7 +1,6 @@
 'use client';
-
 import { ControlPanelDshMarketingScreen } from '@bthwani/app-shells/web/control-panel';
 
 export default function DshMarketingPage() {
-  return <ControlPanelDshMarketingScreen hubHref="/operations/dsh" operationsHref="/operationsions/dsh" operationsHref="/operations" />;
+  return <ControlPanelDshMarketingScreen hubHref="/operations/dsh/marketing" operationsHref="/operations/dsh" />;
 }
