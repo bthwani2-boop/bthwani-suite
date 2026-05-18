5c3af996343e860101faba531d1b1191becaa06b
bthwani2-boop <bthwani2@gmail.com>
2026-05-18 16:28:17 +0300

    chore: checkpoint wlt

--- wlt/frontend/app-client/home/screens/WltHomeGetScreen.tsx
+++ wlt/frontend/app-client/home/screens/WltHomeGetScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useRef, useState } from 'react';
 import { ScrollView, View } from 'react-native';
 import { ScreenWrapper, Card, Text, AmountInput, PaymentMethodList, Button, Icon, TopBar, amountToArabicText, useBThwaniAppearance, useI18n } from '@bthwani/ui-kit';
-import { financeProviders } from '../../../../shared/finance/providers';
+import { financeProviders } from '../../../shared/finance/providers';
 
 export const WltHomeGetScreen: React.FC<{ onBack?: () => void }> = ({ onBack }) => {
   const { t } = useI18n();
--- wlt/frontend/app-client/home/screens/WltTopupScreen.tsx
+++ wlt/frontend/app-client/home/screens/WltTopupScreen.tsx
@@ -2,7 +2,7 @@ import React, { useMemo, useState } from 'react';
 import { ScrollView } from 'react-native';
 import { ScreenWrapper, ScreenState, Text, useI18n, Dialog } from '@bthwani/ui-kit';
 import { AmountInput, PaymentMethodList, SummaryCard, QuickAmountGrid, Button, TextField, amountToArabicText } from '@bthwani/ui-kit';
-import { financeProviders } from '../../../../shared/finance/providers';
+import { financeProviders } from '../../../shared/finance/providers';
 
 export const WltTopupScreen: React.FC = () => {
   const { t } = useI18n();
